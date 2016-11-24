65aa54f74e54fc0f8968035bbb7458b563ce4450
agkphysics <agkphysics@gmail.com>
2016-11-24 23:56:08 +1300

    Added ability for push notifications.

--- FabrikamFoodApp/FabrikamFoodApp.Droid/MainActivity.cs
+++ FabrikamFoodApp/FabrikamFoodApp.Droid/MainActivity.cs
@@ -6,11 +6,10 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
-using Xamarin.Auth;
 
 using Microsoft.WindowsAzure.MobileServices;
 using System.Threading.Tasks;
-using Android.Gms.Gcm;
+using Gcm.Client;
 
 namespace FabrikamFoodApp.Droid
 {
@@ -29,23 +28,26 @@ namespace FabrikamFoodApp.Droid
             {
                 // Sign in with Facebook login using a server-managed flow.
                 user = await AzureManager.CurrentInstance.CurrentClient.LoginAsync(this, MobileServiceAuthenticationProvider.Facebook);
+                if (user != null) success = true;
             }
             catch (Exception ex)
             {
                 message = ex.Message;
+                // Display the success or failure message.
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.SetMessage(message);
+                builder.SetTitle("Sign-in result");
+                builder.Create().Show();
             }
-
-            // Display the success or failure message.
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.SetMessage(message);
-            builder.SetTitle("Sign-in result");
-            builder.Create().Show();
 
             return success;
         }
 
         protected override void OnCreate(Bundle bundle)
         {
+            // Set the current instance of MainActivity.
+            instance = this;
+
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
@@ -55,6 +57,46 @@ namespace FabrikamFoodApp.Droid
             Xamarin.FormsMaps.Init(this, bundle);
             App.Init((IAuthenticate)this);
             LoadApplication(new App());
+
+            try
+            {
+                // Check to ensure everything's setup right
+                GcmClient.CheckDevice(this);
+                GcmClient.CheckManifest(this);
+
+                // Register for push notifications
+                System.Diagnostics.Debug.WriteLine("Registering...");
+                GcmClient.Register(this, PushHandlerBroadcastReceiver.SENDER_IDS);
+            }
+            catch (Java.Net.MalformedURLException)
+            {
+                CreateAndShowDialog("There was an error creating the client. Verify the URL.", "Error");
+            }
+            catch (Exception e)
+            {
+                CreateAndShowDialog(e.Message, "Error");
+            }
+        }
+
+        private void CreateAndShowDialog(String message, String title)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.SetMessage(message);
+            builder.SetTitle(title);
+            builder.Create().Show();
+        }
+
+        // Create a new instance field for this activity.
+        static MainActivity instance = null;
+
+        // Return the current activity instance.
+        public static MainActivity CurrentActivity
+        {
+            get
+            {
+                return instance;
+            }
         }
     }
 }
